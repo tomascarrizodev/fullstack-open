a109d57247a0f40363acb224beac73b98fed0b27
Tomas Carrizo <tomascarrizo.dev@gmail.com>
2024-01-16 12:41:35 -0300

    part2 exercise 2.7

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,16 +3,44 @@ import { useState } from 'react'
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas' }
-  ]) 
+  ])
   const [newName, setNewName] = useState('')
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    const newPersons = [ ...persons, { name: newName } ]
-    setPersons(newPersons)
-    setNewName('')
-  }
 
+    const newPerson = { name: newName }
+    const newPersons = [...persons]
+    newPersons.push(newPerson)
+    
+    // let start = 0
+
+    // while (start < newPersons.length - 1) {
+    //   if (newPersons[start].name === newName) {
+    //     newPersons.pop()
+    //     alert(`${newName} is already in the phonebook`)
+    //     start++
+    //   } else if (newPersons[start] === newPersons.length - 1) {
+    //     setPersons(newPersons)
+    //     start++
+    //   } else if (newPersons[start].name !== newName && newPersons.length - 1 !== start) {
+    //     start++
+    //   }
+    // }
+
+    for (let i = 0; i < newPersons.length - 1; i++) {
+      if (newPersons[i].name === newName) {
+        newPersons.pop()
+        alert(`${newName} is already in the phonebook`)
+      } else if (newPersons[i] === newPersons.length - 1) {
+        setPersons(newPersons)
+        setNewName('')
+      }
+    }
+
+    setPersons(newPersons)
+  }
+    
   const handleChange = (e) => {
     const val = e.target.value
     setNewName(val)
