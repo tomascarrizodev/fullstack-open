f0791966d249f68b40476bc21e5d47bd4839458c
Tomas Carrizo <tomascarrizo.dev@gmail.com>
2024-01-16 16:51:50 -0300

    part2 exercise 2.9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,10 +2,15 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '54-261-3042443', id: 1 }
+    { name: 'Arto Hellas', number: '54-261-3042443', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [search, setSearch] = useState('')
+  const [filteredPersons, setFilteredPersons] = useState(persons)
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -18,25 +23,56 @@ const App = () => {
       if (newPersons[i].name === newName) {
         newPersons.pop()
         alert(`${newName} is already in the phonebook`)
-      } else if (newPersons[i] === newPersons.length - 1) {
-        setPersons(newPersons)
-        setNewName('')
       }
     }
-
-    setPersons(newPersons)
-  }
     
+    setPersons(newPersons)
+    setFilteredPersons(newPersons)
+    setNewName('')
+    setNewNumber('')
+  }
+  
+  const handleSearch = (val) => {
+    const value = val.trim().toLowerCase()
+    const newFilter = [...persons]
+    const lower = newFilter.map(e => {
+      return {...e, name: e.name.toLowerCase()}
+    })
+    const positions = []
+    const filtered = []
+
+    for (let i = 0; i < lower.length; i++) {
+      if (lower[i].name.includes(value))
+        positions.push(i)
+    }
+
+    for (let j = 0; j < newFilter.length; j++) {
+      filtered.push(newFilter[positions[j]])
+    }
+
+    const newFilt = filtered.filter(e => e !== undefined)
+    setFilteredPersons(newFilt)
+
+    if (value === '') {
+      setFilteredPersons(persons)
+    }
+  }
+
   const handleChange = (e, func) => {
     const val = e.target.value
     func(val)
+    if (e.target.parentNode.innerText.includes('filter')) {
+      handleSearch(val)
+    }
   }
-
-  console.log(persons)
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input value={search} onChange={e => handleChange(e, setSearch)} />
+      </div>
+      <h2>Add a new</h2>
       <form onSubmit={(e) => handleSubmit(e)} >
         <div>
           name: <input value={newName} onChange={e => handleChange(e, setNewName)} />
@@ -50,9 +86,11 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       {
-        persons.map(e => {
-          return <p key={e.id}>{e.name} {e.number}</p>
-        })
+        filteredPersons.length ?
+          filteredPersons.map(e => {
+            return <p key={e.id}>{e.name} {e.number}</p>
+          }) : 
+          <p>No matches</p>
       }
     </div>
   )
