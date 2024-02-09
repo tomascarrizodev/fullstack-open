f3486ca31dbb4853b11ed1a739b22e0f3b62168a
Tomas Carrizo <tomascarrizo.dev@gmail.com>
2024-02-08 21:49:19 -0300

    part5 exercises 5.1/5.17

---
+++ part5/bloglist-frontend/src/App.jsx
@@ -0,0 +1,163 @@
+/* eslint-disable linebreak-style */
+import { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import blogService from './services/blogs'
+import loginService from './services/login'
+import NewBlog from './components/NewBlog'
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
+  const [notification, setNotification] = useState('')
+  const [notiLogin, setNotiLogin] = useState('')
+  const [toggleNewBlog, setToggleNewBlog] = useState(true)
+
+  useEffect(() => {
+    blogService.getAll().then(blogs =>
+      setBlogs(blogs)
+    )
+  }, [])
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      setUser(JSON.parse(loggedUserJSON))
+    }
+  }, [])
+
+  useEffect(() => {
+    const noti = setInterval(() => {
+      setNotification('')
+    }, 5000)
+
+    return () => clearInterval(noti)
+  }, [notification])
+
+  useEffect(() => {
+    const noti = setInterval(() => {
+      setNotiLogin('')
+    }, 5000)
+
+    return () => clearInterval(noti)
+  }, [notiLogin])
+
+  const handleLogin = async (e) => {
+    e.preventDefault()
+
+    try {
+      const user = await loginService
+        .login({ username, password })
+      window.localStorage.setItem('loggedBlogappUser', JSON.stringify(user))
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (error) {
+      console.log({ error: error.message })
+      setNotiLogin('wrong username or password')
+    }
+  }
+
+  const handleLogout = () => {
+    setUser(null)
+    window.localStorage.removeItem('loggedBlogappUser')
+  }
+
+  const handleCreate = async (e) => {
+    e.preventDefault()
+    try {
+      const response = await blogService.create({ title, author, url }, user.token)
+
+      if (response.status !== 201) {
+        throw new Error('bad request')
+      }
+
+      const updatedBlogs = await blogService.getAll()
+      setBlogs(updatedBlogs)
+
+      setAuthor('')
+      setTitle('')
+      setUrl('')
+
+      setNotification(`a new blog "${title}" by ${author} added`)
+
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
+
+  const handleDelete = async (id, title, author) => {
+    try {
+      if (window.confirm(`Are you sure you want to delete "${title}" by ${author}?`)) {
+        await blogService
+          .remove(id, user.token)
+        setBlogs(blogs.filter(blog => blog.id !== id))
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  return (
+    <div>
+      <h1>blogs</h1>
+      {user === null
+        ?
+        <div>
+          <h1>log in to application</h1>
+          {notiLogin &&
+            <div style={{ background: 'gray', border: '2px solid red', borderRadius: '8px', padding: '8px', color: 'red' }}>
+              {notiLogin}
+            </div>
+          }
+          <form onSubmit={e => handleLogin(e)}>
+            <span>username</span>
+            <input type='text' onChange={(e) => setUsername(e.target.value)} />
+            <br />
+            <span>password</span>
+            <input type='password' onChange={(e) => setPassword(e.target.value)} />
+            <br />
+            <button type='submit'>Log in</button>
+          </form>
+        </div>
+        :
+        <div>
+          <span>{user.name} logged in</span>
+          <button onClick={handleLogout}>logout</button>
+          {notification &&
+            <div style={{ background: 'gray', border: '2px solid limegreen', borderRadius: '8px', padding: '8px', color: 'limegreen' }}>
+              {notification}
+            </div>
+          }
+          <br />
+          {
+            toggleNewBlog
+              ? <button onClick={() => setToggleNewBlog(!toggleNewBlog)}>new blog</button>
+              : <NewBlog handleCreate={handleCreate} toggleNewBlog={toggleNewBlog} setToggleNewBlog={setToggleNewBlog}>
+                <span>title</span>
+                <input type='text' id='title' value={title} onChange={(e) => setTitle(e.target.value)} placeholder='title' />
+                <br />
+                <span>author</span>
+                <input type='text' id='author' value={author} onChange={(e) => setAuthor(e.target.value)} placeholder='author' />
+                <br />
+                <span>url</span>
+                <input type='text' id='url' value={url} onChange={(e) => setUrl(e.target.value)} placeholder='url' />
+                <br />
+              </NewBlog>
+          }
+          <h2>blogs</h2>
+          {blogs.sort((a, b) => b.likes - a.likes).map(blog =>
+            <Blog key={blog.id} blog={blog} token={user.token} handleDelete={handleDelete} user={user} />
+          )}
+        </div>
+      }
+
+    </div>
+  )
+}
+
+export default App
---
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from 'react'
+import blogService from '../services/blogs'
+import PropTypes from 'prop-types'
+
+const Blog = ({ blog, token, handleDelete, user, mockLikes }) => {
+  const [toggler, setToggler] = useState('view')
+  const [toggle, setToggle] = useState(false)
+  const [likes, setLikes] = useState(0)
+
+  useEffect(() => {
+    setLikes(blog.likes)
+  }, [])
+
+  const handleToggle = () => {
+    toggler === 'view' ? setToggler('hide') : setToggler('view')
+    setToggle(!toggle)
+  }
+
+  const updateLikes = async () => {
+    setLikes(likes + 1)
+    await blogService
+      .update(blog.id, { ...blog, likes: likes + 1 })
+  }
+
+  return (
+    <div style={{ border: '2px solid gray', borderRadius: '8px', padding: '8px 16px', marginBottom: '4px' }}>
+      <p className='titleAndAuthor'> {blog.title} by {blog.author} </p>
+      <button className='toggleButton' onClick={handleToggle}>{toggler}</button>
+      {
+        toggle &&
+        <div>
+          <p className='url'>url: {blog.url}</p>
+          <div>
+            <p className='likes'>likes: {likes}</p>
+            <button onClick={updateLikes}>like</button>
+            <button style={{ display: 'none' }} className='likeButton' onClick={mockLikes}>like</button>
+          </div>
+          <p>{blog.user.name}</p>
+          {
+            blog.user.username === user.username &&
+            <button onClick={() => handleDelete(blog.id, blog.title, blog.author)}>remove</button>
+          }
+        </div>
+      }
+    </div>
+  )
+}
+
+Blog.proptypes = {
+  blog: PropTypes.object.isRequired,
+  token: PropTypes.string.isRequired,
+  handleDelete: PropTypes.func.isRequired,
+  user: PropTypes.object.isRequired
+}
+
+export default Blog
